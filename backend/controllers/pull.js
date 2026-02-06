6095fa30d54b9c382ed3217783d47da7c4b10adb
Prashant Yadav <prash08484@gmail.com>
2026-02-06 17:50:19 +0530

    adding push pull logic

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,43 @@
-async function pullRepo(){
+const fs = require('fs').promises;
+const path = require('path');
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
+
+async function pullRepo() {
+
+    const repoPath = path.resolve(process.cwd(), ".devGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        // extract data from S3
+        const data = await s3.listObjectsV2({
+            Bucket: S3_BUCKET,
+            Prefix: "commits/",
+        }).promise();
+
+        const objects = data.Contents;
+
+        for (const object of objects) {
+            const key = object.Key;
+            const commitDir = path.join(commitsPath, path.dirname(key).split('/').pop());
+
+            await fs.mkdir(commitDir, { recursive: true });
+
+            const params = {
+                Bucket: S3_BUCKET,
+                Key: key,
+            };
+
+            const fileContent = await s3.getObject(params).promise();
+            await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+        }
+
+        console.log("All commits pulled From S3 BUCKET");
+    }
+    catch (err) {
+        console.log("Error during pulling repository: " + err);
+    }
     console.log("Pull Repo");
 }
 
-module.exports={pullRepo};
+module.exports = { pullRepo };
--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,5 +1,39 @@
-async function pushRepo(){
-    console.log("Puhs Repo");
+const fs = require('fs').promises;
+const path = require('path');
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
+async function pushRepo() {
+
+    // extract all the paths req. 
+    const repoPath = path.resolve(process.cwd(), ".devGit");
+    const commitsPath = path.json(repoPath, "commits");
+
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+        // folder level 
+        for (const commitDir of commitDirs) {
+            const commitPath = path.join(commitsPath, commitDir);
+            // read all file 
+            const files = await fs.readdir(commitPath);
+            //   file level 
+            for (const file of files) {
+                const filePath = path.join(commitPath, file);
+                const fileContent = await fs.readFile(filePath);
+                // set parameters 
+                const params = {
+                    Bucket: S3_BUCKET,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent,
+                };
+                // upload to aws
+                await s3.upload(params).promise();
+            }
+        }
+        console.log("All commits pushed to S3");
+    }
+    catch (err) {
+        console.error("Error pusing to S3: ", err);
+    }
 }
 
-module.exports={pushRepo};
+module.exports = { pushRepo };
